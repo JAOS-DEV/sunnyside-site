54389fbfec182e7bfba7456113a6b394ad51b5c3
J <73671839+JAOS-DEV@users.noreply.github.com>
2021-07-10 18:33:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
 
 const toggle_nav = document.querySelector(".toggle_nav_links");
 
-function myFunction() {
+const navID = document.getElementById("toggleNavID");
+
+// window.addEventListener("resize", (e) => {
+//   console.log(e.target.innerWidth);
+//   let windowWidth = e.target.innerWidth;
+//  if()
+// });
+
+function toggleNav() {
   if (toggle_nav.style.display === "block") {
-    toggle_nav.style.display = "none";
+    navID.classList.toggle("toggleNavClass");
   } else {
-    toggle_nav.style.display = "block";
+    navID.classList.toggle("toggleNavClass");
   }
 }
